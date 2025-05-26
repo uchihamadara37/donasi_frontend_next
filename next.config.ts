175794fc3a51f1f8266124d1352f8d477e1fe72d
andre <andreamusholin@gmail.com>
2025-05-26 13:29:34 +0700

    okoko

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,22 @@ import fs from "fs";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        port: '', // Kosongkan jika menggunakan port default (443 untuk https)
+        pathname: '/c-01-450604/**', // Sesuaikan dengan path bucket Anda jika ingin lebih spesifik
+                                     // Atau gunakan '/**' jika ingin mengizinkan semua path di hostname tersebut
+      },
+      // Anda bisa menambahkan pola lain di sini jika ada sumber gambar eksternal lain
+      // {
+      //   protocol: 'https',
+      //   hostname: 'another-domain.com',
+      // },
+    ],
+  },
 };
 
 export default nextConfig;
